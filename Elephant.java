c0e2446ffd6bb8570b08cd446ce9520de09cdce5
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-11-27 12:30:27 -0500

    Add score

--- Elephant.java
+++ Elephant.java
@@ -11,15 +11,18 @@ public class Elephant extends Actor
             move(2);
         }
         
+        eat();
+    }
+    
+    public void eat() {
         // Eat the apple
         if (isTouching(Apple.class)){
             removeTouching(Apple.class);
             MyWorld world = (MyWorld) getWorld();
             world.spawnApple();
+            world.increaseScore();
         }
     }
     
-    // public void move(int x) {
-        // System.out.println("Don't touch me.");
-    // }
+   
 }
--- MyWorld.java
+++ MyWorld.java
@@ -2,13 +2,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
 {
+    private int score = 0;
+    private Label scoreLabel;
+    
     public MyWorld()
     {    
         super(600, 400, 1);
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
         spawnApple();
+        
+        scoreLabel = new Label(0, 70);
+        addObject(scoreLabel, 50, 50);
 
+    }
+    
+    public void increaseScore() {
+        score = score + 1;
+        scoreLabel.setValue(score);
     }
     
     public void spawnApple() {
